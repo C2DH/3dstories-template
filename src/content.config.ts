c3898e8221c7b0cdd85fd426e6e3da3078d93c6f
Daniele Guido <gui.daniele@gmail.com>
2025-08-06 16:58:28 +0200

    add excerpt and footer to strory collection in Astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -18,6 +18,7 @@ const stories = defineCollection({
   loader: glob({ pattern: '*.yaml', base: `./src/content/stories` }),
   schema: z.object({
     title: localizedZodObject(z.string().min(2).max(100)),
+    excerpt: localizedZodObject(z.string().min(10).max(200)),
     content: localizedZodObject(z.string().min(2).max(1000)),
     ordering: z.number().int().min(0).max(1000).optional(),
     duration: z.number().int().min(1).max(1000).optional().default(10),
@@ -27,6 +28,20 @@ const stories = defineCollection({
         content: localizedZodObject(z.string().min(10)),
       })
     ),
+    footer: z
+      .object({
+        title: localizedZodObject(z.string().max(100)),
+        content: localizedZodObject(z.string().max(500)),
+        additionalLinks: z
+          .array(
+            z.object({
+              url: z.string().url(),
+              title: localizedZodObject(z.string().max(100)),
+            })
+          )
+          .optional(),
+      })
+      .optional(),
     settings: z.object({
       // Scene component path inside component/scenes. This component is responsible of
       // displaying the objects. See src/components/scenes/README.md to check
@@ -40,7 +55,8 @@ const stories = defineCollection({
       // The sheet is a JSON file that contains the Theatre.js project data.
       // if there is no sheet, the scene component use Theatre.js local database, named
       // after the story identifier.
-      sheet: z.string().optional(),
+      state: z.string().optional(),
+      editable: z.boolean().optional().default(true),
     }),
   }),
 })
